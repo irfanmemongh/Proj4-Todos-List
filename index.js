3dc65e74362d684319c4d0f5ef7c44e70fae7c69
irfanmemongh <167445390+irfanmemongh@users.noreply.github.com>
2024-05-05 18:43:41 +0500

    Push my Project 4 Todos List to github

---
+++ index.js
@@ -0,0 +1,20 @@
+#! /usr/bin/env node
+//Todos List Project by Irfan Memon
+import inquirer from "inquirer";
+let todos = [];
+let condition = true;
+while (condition) {
+    let addTask = await inquirer.prompt([{
+            name: "todo",
+            type: "input",
+            message: " What you want to add in your Todos List?"
+        }, {
+            name: "addMore",
+            type: "confirm",
+            message: "Do you want to add add more in your Todos List?",
+            default: "false"
+        }]);
+    todos.push(addTask.todo);
+    condition = addTask.addMore;
+    console.log(todos);
+}
